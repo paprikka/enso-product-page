3716ac106272bf59d36749b3e7d8d07d7e472de2
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-05-31 22:02:25 +0100

    chore: social meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { FC } from "react";
 import { Fade } from "../components/fade";
 import { Footer } from "../components/footer";
 import { Hero } from "../components/hero";
@@ -6,19 +7,52 @@ import { Preview } from "../components/preview";
 import { ValueProp } from "../components/value-proposition";
 import styles from "../styles/Home.module.css";
 
+type SocialMetaProps = {
+  description: string;
+  title: string;
+  url: string;
+  image: string;
+};
+
+const SocialMeta: FC<SocialMetaProps> = (props: SocialMetaProps) => (
+  <>
+    <meta property="og:description" content={props.description} />
+    <meta property="og:title" content={props.title} />
+    <meta property="og:site_name" content="enso.sonnet.io" />
+    <meta property="og:url" content="https://enso.sonnet.io" />
+    <meta property="og:image" content={props.image} />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="400" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@rafalpast" />
+    <meta name="twitter:creator" content="@rafalpast" />
+    <meta name="twitter:title" content={props.title} />
+    <meta name="twitter:description" content={props.description} />
+    <meta name="twitter:image" content={props.image} />
+    <title> {props.title} </title>
+    <meta
+      name="description"
+      content={props.description + " Click to learn more."}
+    />
+  </>
+);
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Ensō—write now, edit later.</title>
-        <meta
-          name="description"
-          content="Ensō is a writing tool that helps you enter a state of flow. Click to learn more."
-        />
         <link rel="icon" href="/favicon.jpg" />
         <link
           href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;600&display=swap"
           rel="stylesheet"
+        />
+        <SocialMeta
+          description="Ensō is a writing tool that helps you enter a state of flow."
+          title="Ensō—write now, edit later."
+          image="https://enso.sonnet.io/social-icon.png"
+          url="https://enso.sonnet.io"
         />
       </Head>
 
