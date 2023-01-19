8559b380aac99760673c0808559771d1114f95de
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-01-19 18:29:58 +0000

    wip

--- components/download/index.tsx
+++ components/download/index.tsx
@@ -4,7 +4,16 @@ import Link from "next/link";
 import { ButtonLink } from "../button-link";
 import { Text } from "../text";
 import { Spacer } from "../spacer";
-import { FC } from "react";
+import { FC, useState } from "react";
+import classNames from "classnames";
+
+import {
+  useTransition,
+  useSpringRef,
+  animated,
+  useSpring,
+  config,
+} from "@react-spring/web";
 
 export const Download: FC<{ isVisible: boolean; onClose: () => void }> = ({
   onClose,
@@ -12,52 +21,66 @@ export const Download: FC<{ isVisible: boolean; onClose: () => void }> = ({
 }) => {
   const handleCloseClick = () => onClose();
 
-  if (!isVisible) return null;
+  const overlayTransitions = useTransition([isVisible], {
+    from: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+    },
+    leave: {
+      opacity: 0,
+    },
+  });
 
-  return (
-    <div className={styles.overlay}>
-      <div className={styles.content}>
-        <div className={styles.icon}>
-          <Image
-            alt="Ensō"
-            src={"/app-icon.png"}
-            width={160}
-            height={160}
-            className={styles.icon}
-          />
-        </div>
-        <h2 className={styles.title}>Ensō for Mac</h2>
-        <div className={styles.buttons}>
-          <ButtonLink
-            href="https://3689984635711.gumroad.com/l/ewckz"
-            label="Buy now"
-            level="primaryOutline"
-            noMargin
-            size="s"
-          />
-          <Text dimmed size="s" align="center">
-            - or -
+  return overlayTransitions((overlayStyles, item) =>
+    !item ? null : (
+      <animated.div className={styles.overlay} style={overlayStyles}>
+        <div className={styles.content}>
+          <div className={styles.icon}>
+            <Image
+              alt="Ensō"
+              src={"/app-icon.png"}
+              width={160}
+              height={160}
+              className={styles.icon}
+            />
+          </div>
+          <h2 className={styles.title}>Ensō for Mac</h2>
+          <div className={styles.buttons}>
+            <ButtonLink
+              href="https://3689984635711.gumroad.com/l/ewckz"
+              label="Buy now"
+              level="primaryOutline"
+              noMargin
+              size="s"
+            />
+            <Text dimmed size="s" align="center">
+              - or -
+            </Text>
+            <ButtonLink
+              href="https://write.sonnet.io"
+              label="Try the web version"
+              level="secondaryOutline"
+              noMargin
+              size="s"
+            />
+          </div>
+          <Spacer size={1} />
+          <Text dimmed size="s" inline>
+            free and{" "}
+            <Link href="https://github.com/paprikka/ulysses">
+              always private
+            </Link>
           </Text>
-          <ButtonLink
-            href="https://write.sonnet.io"
-            label="Try the web version"
-            level="secondaryOutline"
-            noMargin
-            size="s"
-          />
+          <Spacer size={6} />
+          <Text size="s" inline>
+            Looking for Windows or Linux support?{" "}
+            <Link href="https://sonnet.io/posts/hi">Get in touch</Link>.
+          </Text>
+          <button className={styles.closeButton} onClick={handleCloseClick} />
         </div>
-        <Spacer size={1} />
-        <Text dimmed size="s" inline>
-          free and{" "}
-          <Link href="https://github.com/paprikka/ulysses">always private</Link>
-        </Text>
-        <Spacer size={6} />
-        <Text size="s" inline>
-          Looking for Windows or Linux support?{" "}
-          <Link href="//">Get in touch</Link>.
-        </Text>
-        <button className={styles.closeButton} onClick={handleCloseClick} />
-      </div>
-    </div>
+      </animated.div>
+    )
   );
 };
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,30 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "../button";
 import { ButtonLink } from "../button-link";
+import { Download } from "../download";
 import { HGroup } from "../h-group";
 import styles from "./index.module.scss";
 
-export const Hero = () => (
-  <section className={styles.container}>
-    <h1 className={styles.headline}>
-      Write now, <br />
-      edit later.
-    </h1>
-    <section className={styles.description}>
-      <p>Ensō is a writing tool that helps you enter a state of flow.</p>
+export const Hero = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
 
-      <p>
-        It does this by separating writing from editing and thus making it
-        harder for you to edit yourself.
-      </p>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(`isVisible: ${isModalVisible}`);
+      setIsModalVisible(!isModalVisible);
+    }, 2000);
 
-      <HGroup>
-        <ButtonLink
-          level="primary"
-          href="https://write.sonnet.io"
-          label="Try it for yourself"
-        />
-        <ButtonLink href="https://sonnet.io/posts/ulysses" label="Learn more" />
-      </HGroup>
-    </section>
-  </section>
-);
+    return () => clearTimeout(timer);
+  }, [isModalVisible]);
+
+  return (
+    <>
+      <section className={styles.container}>
+        <h1 className={styles.headline}>
+          Write now, <br />
+          edit later.
+        </h1>
+        <section className={styles.description}>
+          <p>Ensō is a writing tool that helps you enter a state of flow.</p>
+
+          <p>
+            It does this by separating writing from editing and thus making it
+            harder for you to edit yourself.
+          </p>
+
+          <HGroup>
+            <Button
+              level="primary"
+              onClick={() => setIsModalVisible(true)}
+              label="Try it for yourself"
+            />
+            <ButtonLink
+              href="https://sonnet.io/posts/ulysses"
+              label="Learn more"
+              level="secondary"
+            />
+          </HGroup>
+        </section>
+      </section>
+      <Download
+        isVisible={isModalVisible}
+        onClose={() => {
+          setIsModalVisible(false);
+        }}
+      />
+    </>
+  );
+};
