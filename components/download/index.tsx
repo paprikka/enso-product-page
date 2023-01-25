f5de58e87d2c9fdc0fc17a2112aa8191d4155efe
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-01-25 13:14:10 +0000

    chore: update gumroad URL

--- components/download/index.tsx
+++ components/download/index.tsx
@@ -38,7 +38,7 @@ export const Download: FC<{ isVisible: boolean; onClose: () => void }> = ({
         <h2 className={styles.title}>Ensō for Mac</h2>
         <div className={styles.buttons}>
           <ButtonLink
-            href="https://sonnet.gumroad.com/l/ewckz"
+            href="https://sonnet.gumroad.com/l/enso"
             label="Buy now"
             level="primaryOutline"
             noMargin
